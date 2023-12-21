2a58c08d3846f45fe60503c8c13ba337b1ab66cb
Stefan Smudja <stefan.smudja@consid.se>
2023-12-21 14:39:34 +0100

    made bot runnable without api keys and some logging

--- urbandictionary/urban_dictionary.py
+++ urbandictionary/urban_dictionary.py
@@ -2,11 +2,17 @@ import os
 import aiohttp
 from dotenv import load_dotenv
 
-load_dotenv()
-API_KEY = os.getenv('URBAN_DICTIONARY_TOKEN')
+API_KEY = None
 
+# Try to load the API key from the .env file
+try:
+    load_dotenv()
+    API_KEY = os.getenv('URBAN_DICTIONARY_TOKEN')
+except Exception as init_error:
+    print(f"Could not initialise Urban Dictionary client: {init_error}")
+
+# Network request related variables
 base_url = "https://mashape-community-urban-dictionary.p.rapidapi.com"
-
 headers = {
     'X-RapidAPI-Key': API_KEY,
     'X-RapidAPI-Host': "mashape-community-urban-dictionary.p.rapidapi.com"
@@ -15,7 +21,11 @@ headers = {
 
 async def request_term(term):
     url = base_url + "/define?term=" + term
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers=headers) as response:
-            data = await response.json()
-            return data
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers) as response:
+                data = await response.json()
+                return data
+    except Exception as api_error:
+        print(f"An error occurred: {api_error}")
+        return None
